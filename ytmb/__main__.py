1d4d5323a52cc3d79a15d654dfb16f446d62c046
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2024-02-04 18:51:09 -0800

    Project setup (#14)
    * i dont like the way this works, but i can import things now
    
    * jupyter notebook imports and logging
    
    * logs of all sorts
    
    * package command
    
    * clean up
    
    ---------
    
    Co-authored-by: Teddy1405 <48705917+Teddy1405@users.noreply.github.com>

---
+++ ytmb/__main__.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_const',
+        const=logging.DEBUG,
+        default=logging.INFO,
+    )
+    parser.add_argument('--log', type=Path, default=Path('debug.log'))
+    args = parser.parse_args()
+
+    verbosity_logs = logging.StreamHandler()
+    verbosity_logs.setLevel(args.verbose)
+
+    debug_logs = logging.FileHandler(Path(__file__).parent / 'debug.log')
+    debug_logs.setLevel(logging.DEBUG)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(levelname)s: (%(module)s.%(funcName)s) [%(asctime)s] %(message)s',
+        handlers=[
+            verbosity_logs,
+            debug_logs,
+        ],
+    )
+
+    print("Welcome to YouTube Music Blend!")
+    print(f"You ran this script with {args=}")
+
+if __name__ == '__main__':
+    main()
