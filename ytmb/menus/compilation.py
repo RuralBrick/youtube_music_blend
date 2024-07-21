4013ee9c1bf8c0e4171b1697ddf3ecefb2afe4f7
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2024-07-20 23:08:08 -0700

    compilations (#35)
    Co-authored-by: Teddy1405 <48705917+Teddy1405@users.noreply.github.com>

---
+++ ytmb/menus/compilation.py
@@ -0,0 +1,45 @@
+import logging
+
+from ytmb.ui import create_name_selector, create_playlist_selector
+import ytmb.playlists as pl
+
+
+def compilation_flow():
+    name_selector = create_name_selector()
+    source_playlists = []
+    print("Adding source playlists.")
+    while True:
+        name = name_selector.user_choose()
+        playlist_selector = create_playlist_selector(name)
+        source_playlists.append(playlist_selector.user_choose())
+        titles = '\n'.join(f"- {p['title']}" for p in source_playlists)
+        print(f"Current source playlists:\n{titles}")
+        prompt = "Add another playlist? (y/n) "
+        while (add_another := input(prompt)) not in {'y', 'n'}:
+            print("Please enter 'y' or 'n'.")
+        if add_another == 'n':
+            break
+    print("Choosing target playlist.")
+    name = name_selector.user_choose()
+    playlist_selector = create_playlist_selector(name)
+    target_playlist = playlist_selector.user_choose()
+    print(f"Target playlist: {target_playlist['title']}")
+    logging.info("Getting tracks")
+    source_tracks = [pl.get_tracks(name, p) for p in source_playlists]
+    flat_source_tracks = [t for p in source_tracks for t in p]
+    target_tracks = pl.get_tracks(name, target_playlist)
+    tracks_to_add = pl.tracks_difference(flat_source_tracks, target_tracks)
+    tracks_to_remove = pl.tracks_difference(target_tracks, flat_source_tracks)
+    add_names = '\n\t'.join(t['title'] for t in tracks_to_add)
+    remove_names = '\n\t'.join(t['title'] for t in tracks_to_remove)
+    logging.info(f"Tracks to add:\n\t{add_names}")
+    logging.info(f"Tracks to remove:\n\t{remove_names}")
+    logging.info("Updating playlist")
+    combined_tracks = pl.combine_tracks(
+        source_tracks,
+        pl.SampleLimit.ALL,
+        pl.SampleMethod.IN_ORDER,
+        pl.CombinationMethod.CONCATENATED,
+    )
+    pl.update_playlist(name, target_playlist, combined_tracks)
+    print("Done.")
