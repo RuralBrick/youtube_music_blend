f289c98dc6af706f19e11121f69c51f86522530b
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-08-01 22:57:31 -0700

    basic actions

--- ytmb/menus/tracking.py
+++ ytmb/menus/tracking.py
@@ -1,4 +1,28 @@
+from pathlib import Path
 
+from ytmb.ui import create_name_selector, create_playlist_selector
+import ytmb.playlists as pl
+import ytmb.utils as utils
+from ytmb.exploration import Playlist
+
+
+def get_audits_directory(name: str, playlist: Playlist) -> Path:
+    p_all_audits = utils.get_data_directory(
+        utils.get_config()['tracking']['audits_path']
+    )
+    p_audits = p_all_audits / name / playlist['title']
+    if not p_audits.is_dir():
+        p_audits.mkdir(parents=True)
+    return p_audits
 
 def tracking_flow():
-    pass
+
+    name_selector = create_name_selector()
+    name = name_selector.user_choose()
+
+    playlist_selector = create_playlist_selector(name)
+    playlist = playlist_selector.user_choose()
+
+    get_audits_directory(name, playlist)
+
+    pl.get_tracks(playlist)
